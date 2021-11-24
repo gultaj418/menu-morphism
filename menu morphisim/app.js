4a231b899d37e766e505669cd35bb9bc48e0477c
unknown <gultacat@code.edu.az>
2021-11-24 16:31:58 +0400

    edhwf

--- menu morphisim/app.js
+++ menu morphisim/app.js
@@ -31,42 +31,44 @@ gsap.from(
   "1"
 );
 
-const tl = gsap.timeline();
+const tl = gsap.timeline({ paused: true });
 const menuBtn = document.querySelector(".nav-icon");
 let alfa = 0;
+tl.to(".nav-icon", 0.7, { yPercent: -20, opacity: 0 })
+
+  .to(".nav-bar", 0.5, { xPercent: 20, yPercent: 20 })
+  .to(".nav-bar", 0.9, { xPercent: -120 })
+  .to(".nav-bar", 0.9, { xPercent: 0, zIndex: -99 })
+  .to(".nav-bar", 0.1, { xPercent: 0, yPercent: 0, zIndex: -99 })
+  .to(".nav-icon", 0.7, { yPercent: 0, opacity: 1, zIndex: 99 })
+
+  .to("#second-line", 0.7, { xPercent: -100, opacity: 0 }, "<")
+
+  .to("#first-line", 0.7, { rotation: 45, y: 20, width: 40 }, "<")
+  .to("#third-line", 0.7, { rotation: -45, y: -20, width: 40 }, "<")
+  .fromTo(
+    ".list-element",
+    0.7,
+    { opacity: 0, yPercent: 5 },
+    {
+      opacity: 1,
+      yPercent: 0,
+      stagger: {
+        each: 0.3,
+        from: "from",
+        amount: 2,
+      },
+    }
+  );
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.add("green");
+
   if (alfa == 0) {
-    tl.to(".nav-icon", 0.7, { yPercent: -20, opacity: 0 })
+    tl.play();
 
-      .to(".nav-bar", 0.5, { xPercent: 20, yPercent: 20 })
-      .to(".nav-bar", 0.9, { xPercent: -120 })
-      .to(".nav-bar", 0.9, { xPercent: 0, zIndex: -99 })
-      .to(".nav-bar", 0.1, { xPercent: 0, yPercent: 0, zIndex: -99 })
-      .to(".nav-icon", 0.7, { yPercent: 0, opacity: 1, zIndex: 99 })
-
-      .to("#second-line", 0.7, { xPercent: -100, opacity: 0 }, "<")
-
-      .to("#first-line", 0.7, { rotation: 45, y: 20, width: 40 }, "<")
-      .to("#third-line", 0.7, { rotation: -45, y: -20, width: 40 }, "<");
     alfa = 1;
   } else {
     tl.reverse();
     alfa = 0;
   }
-
-  //   const t2 = gsap.timeline();
-  //   menuBtn.addEventListener("click", () => {
-  //     if (menuBtn.classList.contains("green") == true) {
-  //       //   console.log("hello");
-  //       menuBtn.classList.remove("green");
-  //       t2.to(".nav-icon", 0.7, { yPercent: -20, opacity: 0 })
-  //         .to(".nav-icon", 0.7, { yPercent: 0, opacity: 1, zIndex: 99 })
-  //         .to("#second-line", 0.7, { xPercent: 0, opacity: 1 }, "<")
-  //         .to("#first-line", 0.7, { rotation: 0, y: 0, width: 30 }, "<")
-  //         .to("#third-line", 0.7, { rotation: 0, y: 0, width: 30 }, "<");
-  //       //   .to(".nav-bar", 0.1, { xPercent: 10, yPercent: 10, zIndex: -99 })
-  //       //   .to(".nav-bar", 0.9, { xPercent: 0, zIndex: 99 });
-  //     }
-  //   });
 });
